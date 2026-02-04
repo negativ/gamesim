ff72b25e722a063a8bc7be4cb76d7f9072ded88b
Andrei Karlov <dein.negativ@gmail.com>
2026-02-04 17:33:07 +0200

    More fixes

--- src/Core/Map.cpp
+++ src/Core/Map.cpp
@@ -5,10 +5,6 @@
 
 namespace sw::core {
 
-
-
-
-
 std::uint32_t Map::distance(Position from, Position to) noexcept {
     return std::max(std::abs(std::int32_t(from.x) - std::int32_t(to.x)),
                     std::abs(std::int32_t(from.y) - std::int32_t(to.y)));
